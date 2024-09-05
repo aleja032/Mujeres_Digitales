eb5719689a56f1eea2a8279d419be5b32f73ffd5
Alejandra <alejandra09fajardo@gmail.com>
2024-09-05 10:12:29 -0500

    Aplicando Buenas Practicas

--- MariaAlejandraDelgadoFajardo/Taller_3/src/Components/Form.jsx
+++ MariaAlejandraDelgadoFajardo/Taller_3/src/Components/Form.jsx
@@ -1,66 +1,58 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import Result from "./Result";
 import Candelita from '../assets/candelita.png';
 import '../Syles/Form.css';
 
-function Form(){
-    const [grados, setGrados] = useState({fahrenheit: 0, celsius: 0});
+function Form() {
+    const [grados, setGrados] = useState({ fahrenheit: 0, celsius: 0 });
 
+    const convertToCelsius = (fahrenheit) => (fahrenheit - 32) * (5/9);
+    const convertToFahrenheit = (celsius) => celsius * (9 / 5) + 32;
 
-    const converter = (e) =>{
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
 
-        if(e.target.name === "celsius" && e.target.value != 0){
-            let  fahrenheit =(e.target.value *  9/5) + 32 ;
-
-            setGrados((prevGrados) => ({
-                ...prevGrados, fahrenheit : fahrenheit, celsius : e.target.value
-            }));
-
-            console.log(fahrenheit);
-        }else if (e.target.name === "fahrenheit" && e.target.value != 0){
-            let  celsius =(e.target.value - 32) * 5/9 ;
-
-            setGrados((prevGrados) => ({
-                ...prevGrados, fahrenheit : e.target.value, celsius : celsius
-            }));
-            console.log(e.target.value);
-        }else {
-            setGrados((prevGrados) => ({
-                ...prevGrados, fahrenheit : 0, celsius : 0
-            }));
+        if (name === "celsius" && value !== "") {
+            const fahrenheit = convertToCelsius(Number(value));
+            setGrados({ celsius: value, fahrenheit });
+        } else if ( name === "fahrenheit" && value !== 0) {
+            const celsius = convertToCelsius(Number(value));
+            setGrados({ fahrenheit: value, celsius });
+        } else {
+            setGrados({ fahrenheit: "", celsius: "" });
         }
     }
-    return(
+    return (
         <div className="container  d-flex justify-content-center align-items-center">
             <div className="form">
                 <h3 className="border-bottom p-4 text-center">
                     TEMPERATURE CALCULATOR
                 </h3>
-                <div className="p-3 mt-3 "> 
+                <div className="p-3 mt-3 ">
                     <label htmlFor="celsius" className="mb-2">Enter temperature in degrees celsius: </label>
-                    <input type="number" 
-                    name="celsius"
-                    onChange={converter}
-                    value={grados.celsius != "" ? grados.celsius : ""}
-                    className="py-1 text-center"
+                    <input type="number"
+                        name="celsius"
+                        onChange={handleInputChange}
+                        value={grados.celsius || ""}
+                        className="py-1 text-center"
                     />
                 </div>
                 <div className="text-center"> Or</div>
 
-                <div className="p-3"> 
+                <div className="p-3">
                     <label htmlFor="fahrenheit" className="mb-2">Enter temperature in degrees fahrenheit: </label>
-                    <input type="number" 
+                    <input type="number"
 
-                    name="fahrenheit"
-                    onChange={converter}
-                    value={grados.fahrenheit != "" ? grados.fahrenheit : ""}
-                    className="py-1 text-center"
+                        name="fahrenheit"
+                        onChange={handleInputChange}
+                        value={grados.fahrenheit || ""}
+                        className="py-1 text-center"
 
                     />
                 </div>
-                <Result grados={grados}/>
-            <img src={Candelita} alt="" className="candelita"/>
+                <Result grados={grados} />
+                <img src={Candelita} alt="" className="candelita" />
             </div>
         </div>
     )
-}export default Form
+} export default Form
