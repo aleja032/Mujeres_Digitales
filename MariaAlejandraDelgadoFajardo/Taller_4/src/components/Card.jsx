8d1ab9652d62fa39ef046afc89784758436cee7d
Alejandra <alejandra09fajardo@gmail.com>
2024-10-02 21:34:25 -0500

    Handle advice retrieval using the custom useFetch hook

--- MariaAlejandraDelgadoFajardo/Taller_4/src/components/Card.jsx
+++ MariaAlejandraDelgadoFajardo/Taller_4/src/components/Card.jsx
@@ -1,56 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import  Dado  from '../assets/icon-dice.svg';
-import  DividerDesktop from '../assets/pattern-divider-desktop.svg';
-import  Dividermobile from '../assets/pattern-divider-mobile.svg';
+import React, { useState, useEffect } from 'react';
+import useFetch from '../hook/useFetch.js';
+import Dado from '../assets/icon-dice.svg';
+import DividerDesktop from '../assets/pattern-divider-desktop.svg';
+import Dividermobile from '../assets/pattern-divider-mobile.svg';
 import "../Styles/Card.css";
 
 function Card (){
-
-    const [isMobile, setIsMobile] = useState(false);
-    const [ advice, setAdvice ] = useState([]);  
-    const [ loading, setLoading ] = useState(false);
-    const [ error, setError ] = useState(null);
-
-    const fetchAdvice = async (url) => {
-        setLoading(true);
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            setAdvice(data.slip);
-        } catch (err) {
-            setError(err);
-        } finally {
-            setLoading(false);
-        }
-    };
+    const [url, setUrl] = useState('https://api.adviceslip.com/advice/71'); 
+    const [fetchCount, setFetchCount] = useState(0);  
+    const { data, loading, error } = useFetch(url, fetchCount);
+    const [isMobile, setIsMobile] = useState(false); 
 
     useEffect(() => {
         const handleResize = () => { setIsMobile(window.innerWidth <= 575); };
         handleResize();
         window.addEventListener('resize', handleResize);
-        fetchAdvice('https://api.adviceslip.com/advice/71'); 
-
+        
         return () => {console.log("cambio"), window.removeEventListener('resize', handleResize); };
     }, []);
+    
 
-    const handleAdvice = () => fetchAdvice('https://api.adviceslip.com/advice');
+    const handleAdvice = () => {
+        setUrl('https://api.adviceslip.com/advice');
+        setFetchCount(prevCount => prevCount + 1); 
+    };
 
     return( 
             <div className="card pt-2">
                 <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                    <div className="card-title py-3 pt-lg-4 custom-title"> {advice.id ? `ADVICE #${advice.id}` : 'ADVICE'} </div>
+                    <div className="card-title py-3 pt-lg-4 custom-title">  {loading ? 'ADVICE #' : data && data.id ? `ADVICE #${data.id}` : 'ADVICE'} </div>
                     <div className="card-text text-center mb-4 mx-lg-4 mx-sm-0 ">
 
-                        {loading ? "Cargando el consejo..." : error ? error : `"${advice.advice}"`}
-                        
-                        </div>
+                        {loading ? "Cargando el consejo..." : error ? error : data && `"${data.advice}"`}
+                    </div>
                         <img src={isMobile ? Dividermobile : DividerDesktop} alt="" className='mt-lg-3  mt-sm-0 custom-color-divider'/>
                     <div className="position-relative  d-flex p-4  mt-lg-2  mt-sm-0">
-                        <button className="position-absolute circular" onClick={handleAdvice}>
-                            <img src={Dado} alt="" />
+                        <button className="position-absolute circular"  onClick={handleAdvice} >
+                            <img src={Dado} alt=""/>
                         </button>
                     </div>
                 </div>
             </div>
+            
     )
 }export default Card
